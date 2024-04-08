fa51ca0a6c3d6f2bc96f466438b877258d6999e9
Bryce Eadie <bryce.eadie@datadoghq.com>
2024-04-07 17:20:35 -0700

    Add course solution

--- Mail Merge Project/main.py
+++ Mail Merge Project/main.py
@@ -7,13 +7,31 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-with open("Input/Letters/starting_letter.txt", mode="r") as starting_letter:
-    # print(starting_letter.readlines())
-    names = open("Input/Names/invited_names.txt")
-    starting_letter_list = starting_letter.readlines()
+# My solution
+
+# with open("Input/Letters/starting_letter.txt", mode="r") as starting_letter:
+#     # print(starting_letter.readlines())
+#     names = open("Input/Names/invited_names.txt")
+#     starting_letter_list = starting_letter.readlines()
+#     for name in names:
+#         scrubbed_name = name.replace("\n", "")
+#         new_file = open(f"Output/ReadyToSend/{scrubbed_name}.txt", mode="w")
+#         for line in starting_letter_list:
+#             new_file.write(line.replace("[name]", scrubbed_name))
+#         new_file.close()
+
+# Course solution
+
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+    print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
     for name in names:
-        scrubbed_name = name.replace("\n", "")
-        new_file = open(f"Output/ReadyToSend/{scrubbed_name}.txt", mode="w")
-        for line in starting_letter_list:
-            new_file.write(line.replace("[name]", scrubbed_name))
-        new_file.close()
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
